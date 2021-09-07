55fd53f1cc23bc4649bd4e547d4828139e3cda98
olaoluwa <oajbade.devtest@gmail.com>
2021-09-06 22:53:05 -0400

    message7

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('i will be there'));
+app.get('/', (req, res) => res.send('i will be home soon'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
